080b8c4b6a129b32f3e956ec4591a8e52e4922a4
gykim_kr <k_bright_st@pusan.ac.kr>
2026-01-13 17:36:59 +0900

    종이의 개수 풀이 완료 - 분할정복/재귀 문제로 어렵지 않았음. - 색종이 만들기는 종이의 개수 하위호환 문제.

---
+++ Barkingdog/0x0B Recursive/BOJ_1780/1780_종이의 개수.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N;
+int ans[3];
+
+void recursive(vector<vector<int>> &arr, pair<int, int> start, int size) {
+    if (size == 1) {
+        ans[arr[start.first][start.second]]++;
+        return;
+    }
+    bool check = false;
+    int num = arr[start.first][start.second];
+    for (int i = start.first; i < start.first + size; ++i) {
+        for (int j = start.second; j < start.second + size; ++j) {
+            if (num != arr[i][j]) {
+                check = true;
+                break;
+            }
+            num = arr[i][j];
+        }
+        if (check) break;
+    }
+
+    if (check) {
+        int new_size = size / 3;
+        recursive(arr, {start.first, start.second}, new_size);
+        recursive(arr, {start.first, start.second + new_size}, new_size);
+        recursive(arr, {start.first, start.second + 2 * new_size}, new_size);
+        recursive(arr, {start.first + new_size, start.second}, new_size);
+        recursive(arr, {start.first + new_size, start.second + new_size}, new_size);
+        recursive(arr, {start.first + new_size, start.second + 2 * new_size}, new_size);
+        recursive(arr, {start.first + 2 * new_size, start.second}, new_size);
+        recursive(arr, {start.first + 2 * new_size, start.second + new_size}, new_size);
+        recursive(arr, {start.first + 2 * new_size, start.second + 2 * new_size}, new_size);
+    } else ans[num]++;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> N;
+    vector<vector<int>> arr(N, vector<int>(N, 0));
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            cin >> arr[i][j];
+            arr[i][j]++;
+        }
+
+    }
+
+    recursive(arr, {0, 0}, N);
+    for (auto i: ans) cout << i << "\n";
+
+    return 0;
+}
