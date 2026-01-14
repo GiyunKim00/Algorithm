92c6ee3e5cb1211e46099a382a4d12fa3c2beb8d
gykim_kr <k_bright_st@pusan.ac.kr>
2026-01-14 21:04:52 +0900

    N과 M(8) 풀이완료 - 어렵지 않았음.

---
+++ Barkingdog/0x0C Back tracking/BOJ_15657/15657_N과 M (8).cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int N, M;
+vector<int> ans, arr;
+
+void recursive(int n) {
+    if (n == M) {
+        for (auto i: ans) cout << i << " ";
+        cout << "\n";
+
+        return;
+    }
+
+    for(auto i : arr) {
+        if(!ans.empty() && ans.back() > i) continue;
+        ans.push_back(i);
+        recursive(n + 1);
+        ans.pop_back();
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> N >> M;
+
+    for (int i = 0; i < N; ++i) {
+        int t;
+        cin >> t;
+        arr.push_back(t);
+    }
+
+    sort(arr.begin(), arr.end());
+
+    recursive(0);
+
+    return 0;
+}
