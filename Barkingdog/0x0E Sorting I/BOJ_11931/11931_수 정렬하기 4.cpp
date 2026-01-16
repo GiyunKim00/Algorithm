3040b4bb59eda4a6b43b5502244f335e1626d9c5
gykim_kr <k_bright_st@pusan.ac.kr>
2026-01-16 16:11:55 +0900

    수 정렬하기 4, 5 풀이완료 - 정렬 문제로, 전혀 어렵지 않음. - algorithm의 sort함수의 사용법을 제대로 익힘.

---
+++ Barkingdog/0x0E Sorting I/BOJ_11931/11931_수 정렬하기 4.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool compare(int x, int y) {
+    return x > y;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int N;
+    cin >> N;
+    vector<int> arr(N);
+    for (int i = 0; i < N; ++i) cin >> arr[i];
+
+    sort(arr.begin(), arr.end(), compare);
+    for(auto i : arr) cout << i <<"\n";
+    return 0;
+}
