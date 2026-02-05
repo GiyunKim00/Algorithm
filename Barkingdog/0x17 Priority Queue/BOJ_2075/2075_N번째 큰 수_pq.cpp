c99b633e884369d09d89f51bd3683aacab95f983
cloud-23k <k_bright_st@pusan.ac.kr>
2026-02-05 22:02:48 +0900

    최소 힙 학습 완료 - STL priority_queue 활용 문제. - 시간/공간 성능 비교를 위해 vector와 queue방식 풀이를 동시 진행. 결과는 queue가 압도적. - 이 떼, queue의 경우 size가 일정 범위를 넘어갈 수 pop 연산을 수행해주는 방식으로 공간복잡도를 제어. - 재밌는 건, size 비교가 빈번한데 q.size()를 int로 typecasting 해주면 속도가 100ms나 단축됨. - 현대 CPU는 보통 signed int 비교에 최적화되어있는데, q.size()>n을 할 시 int->unsigned로 변환하게 됨. 따라서 이를 줄이고자 q.size를 강제로 int typecasting하여 오버헤드를 줄이는 것. - 주석 추가

--- Barkingdog/0x17 Priority Queue/BOJ_2075/2075_N번째 큰 수_pq.cpp
+++ Barkingdog/0x17 Priority Queue/BOJ_2075/2075_N번째 큰 수_pq.cpp
@@ -14,6 +14,11 @@ int main() {
     for (int i = 0; i < n*n; ++i){
         cin >> a;
         q.push(a);
+        /**
+         * size 비교가 빈번한데 q.size()를 int로 typecasting 해주면 속도가 100ms나 단축됨.
+         * 현대 CPU는 보통 signed int 비교에 최적화되어있는데, q.size()>n을 할 시 int->unsigned로 변환하게 됨.
+         * 따라서 이를 줄이고자 q.size를 강제로 int typecasting하여 오버헤드를 줄이는 것.
+         */
         if ((int)q.size() > n) q.pop();
     }
     cout << q.top();
