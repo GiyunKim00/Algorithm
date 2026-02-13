8c225f354290f945a98720ef39b19948556e144f
cloud-23k <k_bright_st@pusan.ac.kr>
2026-02-13 21:55:20 +0900

    상근이의 여행 학습 완료 - 트리 BFS, MST로 풀 수는 있으나 단순한 간선 세기 문제임. - 모든 지역을 최소 간선으로 여행하려면 지역 - 1이 답임. 따라서 n-1.

---
+++ Barkingdog/0x1B Minimum Spanning Tree/BOJ_9372/9372_상근이의 여행.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--){
+        int n, m;
+        cin >> n >> m;
+        cout << n - 1 << "\n";
+        while (m--){
+            int a, b;
+            cin >> a >> b;
+        }
+    }
+
+    return 0;
+}
