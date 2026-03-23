86897b8169c063e402d647d2a2165cf630957c8d
gykim_kr <k_bright_st@pusan.ac.kr>
2026-03-24 00:39:36 +0900

    N과 M (7) 복습 완료 - 백트래킹(재귀) - 어려운 것은 없었음

---
+++ Barkingdog/0x0C Back tracking/BOJ_15656/15656_N과 M (7)_복습.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n, m;
+vector<int> arr, ans;
+
+void recursive(const int size) {
+    if (size == m) {
+        for (const auto i: ans) cout << i << " ";
+        cout << "\n";
+
+        return;
+    }
+
+    for (int i = 0; i < n; ++i) {
+        ans.push_back(arr[i]);
+        recursive(size + 1);
+        ans.pop_back();
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> n >> m;
+
+    arr.reserve(n);
+    ans.reserve(n);
+
+    for (int i = 0; i < n; ++i) {
+        int a;
+        cin >> a;
+        arr.push_back(a);
+    }
+
+    sort(arr.begin(), arr.end());
+
+    recursive(0);
+
+    return 0;
+}
